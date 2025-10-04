cb2abfd73abab53c6ab600d341fbcda6c60d8140
exwestafrican <oladele.alade@gmail.com>
2025-10-04 18:15:52 +0100

    add constants file and fix import path

--- src/features/file-upload/api/usePostStatement.ts
+++ src/features/file-upload/api/usePostStatement.ts
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from "@/lib/api"
+import { API_BASE_URL } from "@/constants"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import axios from "axios"
 
