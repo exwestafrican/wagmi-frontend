3a5d6cca6647e066c049d02db8ca0c7d08a3a244
exwestafrican <oladele.alade@gmail.com>
2025-10-04 17:12:37 +0100

    update query client config

--- src/main.tsx
+++ src/main.tsx
@@ -16,16 +16,7 @@ import reportWebVitals from "./reportWebVitals.ts"
 import UploadPage from "@/features/file-upload/upload-page"
 
 // Create a client
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			staleTime: 1000 * 60 * 5, // 5 minutes
-			gcTime: 1000 * 60 * 30, // 30 minutes (formerly cacheTime)
-			retry: 1,
-			refetchOnWindowFocus: false,
-		},
-	},
-})
+const queryClient = new QueryClient({})
 
 const rootRoute = createRootRoute({
 	component: () => (
