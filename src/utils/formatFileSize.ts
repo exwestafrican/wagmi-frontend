bfba87c30808195ba87879869253231c69d9e37f
exwestafrican <oladele.alade@gmail.com>
2025-10-04 18:05:13 +0100

    fix lint

--- src/utils/formatFileSize.ts
+++ src/utils/formatFileSize.ts
@@ -3,10 +3,8 @@ import type { StatementFile } from "@/features/file-upload/types/statementFile"
 export function formatFileSize(file: StatementFile) {
 	const mbSize = file.size / (1024 * 1024)
 	const kbSize = file.size / 1024
-	debugger
 	if (kbSize >= 1024) {
-		return mbSize.toFixed(2) + " MB"
-	} else {
-		return kbSize.toFixed(2) + " KB"
+		return `${mbSize.toFixed(2)} MB`
 	}
+	return `${kbSize.toFixed(2)} KB`
 }
