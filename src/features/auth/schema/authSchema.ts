b612744da3fd5c3ecc6f4be4d7d19d9d221ebad0
Laura Ogbechie <ogbechielaura04@gmail.com>
2025-12-12 11:42:31 +0100

    creathed schema for auth (signup and login)

---
+++ src/features/auth/schema/authSchema.ts
@@ -0,0 +1,24 @@
+import z from "zod"
+
+export const signupSchema = z.object({
+	firstName: z.string().nonempty({ message: "First name is required" }),
+	lastName: z.string().nonempty({ message: "Last name is required" }),
+	companyName: z.string().nonempty({ message: "Company name is required" }),
+	workEmail: z
+		.email({ message: "Invalid email address, please try again" })
+		.nonempty({ message: "Work email is required" }),
+	phoneNumber: z
+		.string()
+		.nonempty({ message: "Phone Number is required" })
+		.regex(/^(\+234|0)[789][01]\d{8}$/, "Please enter valid phone number"),
+})
+
+export type SignupData = z.infer<typeof signupSchema>
+
+export const loginSchema = z.object({
+	workEmail: z
+		.email({ message: "Invalid email address, please try again" })
+		.nonempty({ message: "Work email is required" }),
+})
+
+export type LoginData = z.infer<typeof loginSchema>
