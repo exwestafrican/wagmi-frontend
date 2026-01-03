3ce85c0c274ade6acd46be370cb3b7fdd893f07f
Laura Ogbechie <ogbechielaura04@gmail.com>
2026-01-03 18:52:50 +0100

    rf: added form validation for feature feedback

---
+++ src/features/waitlist/schema/feature-feedback.ts
@@ -0,0 +1,13 @@
+import z from "zod"
+
+export const featureFeedbackSchema = z.object({
+	feedback: z
+		.string()
+		.nonempty({
+			message: "Please provide your feedback",
+		})
+		.max(5000, {
+			message: "Feedback must be less than 5000 characters.",
+		}),
+	// featureId: z.string() // so we know what feature has this feedback
+})
