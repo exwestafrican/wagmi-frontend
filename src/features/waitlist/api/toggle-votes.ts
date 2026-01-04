2a4f38e5b990d9ada2142e582f2e1ce7f35a3c98
exwestafrican <oladele.alade@gmail.com>
2026-01-04 17:03:40 +0000

    we send users votes to the backend

---
+++ src/features/waitlist/api/toggle-votes.ts
@@ -0,0 +1,27 @@
+import { API_BASE_URL } from "@/constants"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import axios from "axios"
+import { ROADMAP_FEATURES } from "@/features/waitlist/api/roadmap-features.ts"
+import { USER_VOTES } from "@/features/waitlist/api/user-votes"
+
+interface VoteOnFeaturePayload {
+	email: string
+	featureId: string
+}
+
+export function useToggleVotes() {
+	const queryClient = useQueryClient()
+	return useMutation({
+		mutationFn: (payload: VoteOnFeaturePayload) => {
+			console.log("making api call")
+			return axios.post(`${API_BASE_URL}/roadmap/vote`, payload)
+		},
+		onSuccess: async () => {
+			await queryClient.invalidateQueries({ queryKey: [ROADMAP_FEATURES] })
+			await queryClient.invalidateQueries({ queryKey: [USER_VOTES] }) // Add this!
+		},
+		onError: (error) => {
+			console.error("Mutation error:", error)
+		},
+	})
+}
