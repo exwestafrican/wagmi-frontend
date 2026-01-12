5de326e63f44c46889ed26eb3b8a774b5b4e7f13
exwestafrican <oladele.alade@gmail.com>
2026-01-12 20:05:44 +0000

    remove login logic

--- src/features/auth/schema/loginSchema.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod"
-
-export const loginSchema = z.object({
-	workEmail: z
-		.email({ message: "Invalid email address, please try again" })
-		.nonempty({ message: "Work email is required" }),
-})
-
-export type LoginData = z.infer<typeof loginSchema>
