7c6de42676404770d310fe37e7d36e46ace35854
exwestafrican <oladele.alade@gmail.com>
2026-01-16 22:40:31 +0000

    fix rerender bug

--- src/features/auth/signup-page.tsx
+++ src/features/auth/signup-page.tsx
@@ -30,7 +30,7 @@ const SignupPage = () => {
 
 	const form = useForm<SignupData>({
 		resolver: zodResolver(signupSchema),
-		mode: "onBlur",
+		mode: "onChange",
 		defaultValues: {
 			firstName: "",
 			lastName: "",
@@ -59,111 +59,107 @@ const SignupPage = () => {
 		})
 	}
 
-	function SignupComponent() {
-		return (
-			<div className="w-full flex flex-col items-center py-3 px-10">
-				<div className=" w-full text-center">
-					<h2 className="text-4xl font-extrabold mb-3">Envoye</h2>
-					<div>
-						<p>Create an account</p>
-					</div>
-				</div>
-				<Form {...form}>
-					<form
-						onSubmit={form.handleSubmit(onSubmit)}
-						className="space-y-4 w-full max-w-175 flex flex-col gap-6"
-					>
-						<FormField
-							control={form.control}
-							name="firstName"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>First Name</FormLabel>
-									<FormControl>
-										<Input
-											data-testid="first-name"
-											placeholder="John"
-											{...field}
-										/>
-									</FormControl>
-									<FormMessage data-testid="firstname-form-message" />
-								</FormItem>
-							)}
-						/>
-
-						<FormField
-							control={form.control}
-							name="lastName"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Last Name</FormLabel>
-									<FormControl>
-										<Input
-											data-testid="last-name"
-											placeholder="Doe"
-											{...field}
-										/>
-									</FormControl>
-									<FormMessage data-testid="lastname-form-message" />
-								</FormItem>
-							)}
-						/>
-
-						<FormField
-							control={form.control}
-							name="email"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Work Email</FormLabel>
-									<FormControl>
-										<Input
-											data-testid="email"
-											placeholder="jd@xxx.com"
-											{...field}
-										/>
-									</FormControl>
-									<FormDescription className="text-xs">
-										In the absence of work email, please provide personal email
-										address
-									</FormDescription>
-									<FormMessage data-testid="email-form-message" />
-								</FormItem>
-							)}
-						/>
-
-						<FormField
-							control={form.control}
-							name="companyName"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Company Name</FormLabel>
-									<FormControl>
-										<Input
-											data-testid="company-name"
-											placeholder="Doe.inc"
-											{...field}
-										/>
-									</FormControl>
-									<FormMessage data-testid="companyname-form-message" />
-								</FormItem>
-							)}
-						/>
-
-						<Button
-							disabled={!form.formState.isValid}
-							type="submit"
-							className="w-full"
-							data-testid="submit-button"
-						>
-							Sign Up
-						</Button>
-					</form>
-				</Form>
-			</div>
-		)
+	if (signupSuccessful) {
+		return <SignupSuccess />
 	}
 
-	return signupSuccessful ? <SignupSuccess /> : <SignupComponent />
+	return (
+		<div className="w-full flex flex-col items-center py-3 px-10">
+			<div className=" w-full text-center">
+				<h2 className="text-4xl font-extrabold mb-3">Envoye</h2>
+				<div>
+					<p>Create an account</p>
+				</div>
+			</div>
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="space-y-4 w-full max-w-175 flex flex-col gap-6"
+				>
+					<FormField
+						control={form.control}
+						name="firstName"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>First Name</FormLabel>
+								<FormControl>
+									<Input
+										data-testid="first-name"
+										placeholder="John"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage data-testid="firstname-form-message" />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						control={form.control}
+						name="lastName"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Last Name</FormLabel>
+								<FormControl>
+									<Input data-testid="last-name" placeholder="Doe" {...field} />
+								</FormControl>
+								<FormMessage data-testid="lastname-form-message" />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Work Email</FormLabel>
+								<FormControl>
+									<Input
+										data-testid="email"
+										placeholder="jd@xxx.com"
+										{...field}
+									/>
+								</FormControl>
+								<FormDescription className="text-xs">
+									In the absence of work email, please provide personal email
+									address
+								</FormDescription>
+								<FormMessage data-testid="email-form-message" />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						control={form.control}
+						name="companyName"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Company Name</FormLabel>
+								<FormControl>
+									<Input
+										data-testid="company-name"
+										placeholder="Doe.inc"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage data-testid="companyname-form-message" />
+							</FormItem>
+						)}
+					/>
+
+					<Button
+						disabled={!form.formState.isValid}
+						type="submit"
+						className="w-full"
+						data-testid="submit-button"
+					>
+						Sign Up
+					</Button>
+				</form>
+			</Form>
+		</div>
+	)
 }
 
 export default SignupPage
