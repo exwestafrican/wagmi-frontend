ed6ff891c17c50ad1e6eb51624ce66c2179cc616
exwestafrican <oladele.alade@gmail.com>
2026-01-16 22:40:23 +0000

    fix rerender bug

--- src/features/auth/schema/signupSchema.ts
+++ src/features/auth/schema/signupSchema.ts
@@ -6,19 +6,19 @@ export const signupSchema = z.object({
 		.trim()
 		.toLowerCase()
 		.min(2, { message: "First name must contain at least 2 characters" })
-		.max(18),
+		.max(18, { message: "First name too large" }),
 	lastName: z
 		.string()
 		.trim()
 		.toLowerCase()
 		.min(2, { message: "Last name must contain at least 2 characters" })
-		.max(18),
+		.max(18, { message: "Last name too large" }),
 	companyName: z
 		.string()
 		.trim()
 		.toLowerCase()
 		.min(5, { message: "Company name must contain at least 5 characters" })
-		.max(50, { message: "Company name cannot be more than 50 characters" }),
+		.max(50, { message: "Company name is too large" }),
 
 	email: z
 		.email({ message: "Invalid email address, please try again" })
