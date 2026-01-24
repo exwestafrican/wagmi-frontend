213bec06581aa4b523c3cd160a5c09bac7e4a4f1
exwestafrican <oladele.alade@gmail.com>
2026-01-24 17:24:40 +0000

    clean up tests

---
+++ src/test/helpers/mocks.ts
@@ -0,0 +1,14 @@
+import { AxiosError, HttpStatusCode } from "axios"
+
+export function mockError(statusCode: HttpStatusCode) {
+	const code = "ERR_BAD_REQUEST"
+	const axiosError = new AxiosError(
+		"request failed with status code 401",
+		code,
+		undefined,
+		{},
+		undefined,
+	)
+	axiosError.status = statusCode
+	return axiosError
+}
