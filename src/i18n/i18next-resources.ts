102036a22748f67293de7ad60f9bd11b09875762
exwestafrican <oladele.alade@gmail.com>
2026-03-15 19:49:21 +0000

    add French translation

--- src/i18n/i18next-resources.ts
+++ src/i18n/i18next-resources.ts
@@ -3,6 +3,7 @@ import enWaitlist from "@/i18n/translations/en/waitlist.json"
 import enWorkspace from "@/i18n/translations/en/workspace.json"
 import frCommon from "@/i18n/translations/fr/common.json"
 import frWaitlist from "@/i18n/translations/fr/waitlist.json"
+import frWorkspace from "@/i18n/translations/fr/workspace.json"
 
 type Locale = "en" | "fr"
 
@@ -15,7 +16,7 @@ const enResources = {
 const frResources = {
 	common: frCommon,
 	waitlist: frWaitlist,
-	workspace: enWorkspace,
+	workspace: frWorkspace,
 }
 
 const i18nResources = {
