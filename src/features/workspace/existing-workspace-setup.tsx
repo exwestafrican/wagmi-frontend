4998b9784578a89d6ae25d4d25151a689bc360fe
Akomere Akpoveso <akomere.peter@gmail.com>
2026-08-05 03:47:58 +0100

    clean up code

--- src/features/workspace/existing-workspace-setup.tsx
+++ src/features/workspace/existing-workspace-setup.tsx
@@ -29,29 +29,21 @@ export function ExistingWorkspaceSetup() {
 
 	useEffect(() => {
 		if (invalidLink) return
-		// TODO load a bunch of things
-		console.log('called', accessToken);
-		const id = setTimeout(() => {
+		setTimeout(() => {
 			setIsCompleted(true)
 			navigate({
 				to: Pages.WORKSPACE,
 				search: { code: code },
 			}).then()
 		}, 1000)
-
-		//in strict mode react renders component twice, this made the setTimeout call to be called twice
-		//when the second call is triggered we have already navigated to workspaceLayout page, validated the token exists in the url and cleared from the hash param.
-		//This second timeout function call triggers a new navigation again to the workspaceLayout page but when we check the url this time at onBeforeLoad of the workspaceLayout, the access_token is not present, causing redirect to login page.
-		return () => clearTimeout(id);
-	}, [accessToken, code, setIsCompleted, navigate])
+	}, [accessToken, code, navigate])
 
 	useEffect(() => {
 		if (invalidLink && isFinished) {
-			console.log("we are hitting here at some point, so token is getting invalidated")
 			setIsCompleted(true)
 			navigate({ to: Pages.LOGIN }).then()
 		}
-	}, [navigate, isFinished])
+	}, [navigate, invalidLink, isFinished])
 
 	return invalidLink ? (
 		<InvalidLoginLink
